4542b9eab30f5fc8cd104625cd57d4c65ec8e246
Casey M <caseym@gmail.com>
2022-12-05 10:22:25 -0600

    handle database url

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ import dashboard
 
 app = Flask(__name__)
 
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.register_blueprint(auth.views.auth_blueprint)
