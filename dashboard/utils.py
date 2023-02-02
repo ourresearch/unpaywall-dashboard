a896ab9450700a08be6252d047c68d44260fc4f3
Casey M <caseym@gmail.com>
2023-02-02 12:35:19 -0600

    fix bug

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -14,16 +14,22 @@ def is_doi_manually_closed(doi):
 
 def insecure_url(result):
     if result and "best_oa_location" in result and result["best_oa_location"]:
-        if "url" in result["best_oa_location"] and result["best_oa_location"][
-            "url"
-        ].startswith("http://"):
+        if (
+            "url" in result["best_oa_location"]
+            and result["best_oa_location"]["url"]
+            and result["best_oa_location"]["url"].startswith("http://")
+        ):
             return result["best_oa_location"]["url"]
-        elif "url_for_pdf" in result["best_oa_location"] and result["best_oa_location"][
-            "url_for_pdf"
-        ].startswith("http://"):
+        elif (
+            "url_for_pdf" in result["best_oa_location"]
+            and result["best_oa_location"]["url_for_pdf"]
+            and result["best_oa_location"]["url_for_pdf"].startswith("http://")
+        ):
             return result["best_oa_location"]["url_for_pdf"]
-        elif "url_for_landing_page" in result["best_oa_location"] and result[
-            "best_oa_location"
-        ]["url_for_landing_page"].startswith("http://"):
+        elif (
+            "url_for_landing_page" in result["best_oa_location"]
+            and result["best_oa_location"]["url_for_landing_page"]
+            and result["best_oa_location"]["url_for_landing_page"].startswith("http://")
+        ):
             return result["best_oa_location"]["url_for_landing_page"]
     return None
